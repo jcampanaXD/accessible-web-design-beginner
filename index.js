df5fbfbf193c0e1a45906a1ebefe8a5a46c71743
jcampanaXD <jcampana@ufl.edu>
2025-07-01 00:49:58 -0400

    attempt 3 to fix navbar to fit mobile screens (hamburger button)

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const toggleButton = document.getElementById("menuToggle");
+const mainMenu = document.getElementById("mainMenu");
+
+toggleButton.addEventListener("click", () => {
+    mainMenu.classList.toggle("active");
+});
+
 window.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", this.window.scrollY > 50);
