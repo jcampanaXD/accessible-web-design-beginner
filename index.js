6554a726ffe939d69fb454be22cc4bca62691816
jcampanaXD <j.campana@ufl.edu>
2025-07-01 12:51:48 -0400

    fixed all navbar/ ham button issues and added ham icon change to x upon clicking

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const toggleButton = document.querySelector(".menu-toggle");
 const mainMenu = document.querySelector(".main-menu");
+
 toggleButton.addEventListener("click", () => {
     mainMenu.classList.toggle("active");
+    toggleButton.classList.toggle("open");
 });
 
-const navbar = document.getElementById("navbar");
 window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
     navbar.classList.toggle("scrolled", this.window.scrollY > 50);
 });
